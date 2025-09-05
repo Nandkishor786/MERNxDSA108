403199a15da4a0816e0690b314fa9d29d57cebda
nandkishor pal <npcoder0@gmail.com>
2025-09-05 23:49:54 +0530

    Remove submodule and add actual Day02 folder

---
+++ MERN_STACK/Day02/PaytmClone/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        nilla:"#00bef2",
+        darknilla:"#002970",
+        lightcolor:"#f5f5f5",
+        lightnilla:"#00afe3",
+        lightdarknilla:"#0f4a8a",
+        bgcolor:"#f5f7fa",
+      }
+    },
+  },
+  plugins: [],
+}
+
